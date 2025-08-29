98fb3b6367eb37b6e9efa860a8934015b633c4f6
Mars5427 <821374295@qq.com>
2025-08-29 23:06:43 +1000

    Update JSON.vue to use localStorage.

--- src/components/JSON.vue
+++ src/components/JSON.vue
@@ -16,12 +16,24 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="container mt-4">
-    <h2>Authors</h2>
-    <ul class="list-group">
-      <li v-for="author in authors" :key="author.id" class="list-group-item">
-        {{ author.name }} ({{ author.birthYear }})
-      </li>
-    </ul>
+  <div class="container-fluid mt-4">
+    <h2 class="text-center mb-4">📚 Authors List</h2>
+    <div class="row">
+      <div v-for="author in authors" :key="author.id" class="col-md-4 mb-3">
+        <div class="card h-100">
+          <div class="card-body">
+            <h5 class="card-title">{{ author.name }}</h5>
+            <p class="card-text">Born: {{ author.birthYear }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-</template> 
+</template>
+
+<style scoped>
+.card {
+  border-radius: 10px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+}
+</style>
